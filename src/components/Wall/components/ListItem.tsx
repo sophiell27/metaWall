951aa49562041e3098bfb9edcee91d791e064512
sophiee <105938870+sophiell27@users.noreply.github.com>
2024-06-20 23:38:35 +0800

    chore: remove unuse code

--- src/components/Wall/components/ListItem.tsx
+++ src/components/Wall/components/ListItem.tsx
@@ -1,29 +1,25 @@
-import { PostTypes } from '../Wall';
+import { IPost } from '../../../reactQuery/hooks/post/usePosts';
 import Photo from './Avatar';
 
-const ListItem = ({ item }: { item: PostTypes }) => {
-    const { user, createdAt, content, imageUrl } = item;
-    return (
-        <div className='p-6 itemWrapper'>
-            <div className='flex items-center gap-x-3 mb-4'>
-                <Photo size='h-11 w-11'>
-                    {user.photo && <img src={user.photo} alt='' />}
-                </Photo>
-                <div className=''>
-                    <h4>{user.name}</h4>
-                    <small>{createdAt.toLocaleString()}</small>
-                </div>
-            </div>
-            <article className='mb-4 text-start'>{content}</article>
-            {imageUrl && (
-                <img
-                    src={imageUrl}
-                    alt='post'
-                    className='w-full rounded-default'
-                />
-            )}
+const ListItem = ({ item }: { item: IPost }) => {
+  const { user, createdAt, content, imageUrl } = item;
+  return (
+    <div className='p-6 itemWrapper'>
+      <div className='flex items-center gap-x-3 mb-4'>
+        <Photo size='h-11 w-11'>
+          {user.photo && <img src={user.photo} alt='' />}
+        </Photo>
+        <div className='text-left'>
+          <h4>{user.name}</h4>
+          <small>{createdAt.toLocaleString()}</small>
         </div>
-    );
+      </div>
+      <article className='mb-4 text-start'>{content}</article>
+      {imageUrl && (
+        <img src={imageUrl} alt='post' className='w-full rounded-default' />
+      )}
+    </div>
+  );
 };
 
 export default ListItem;
