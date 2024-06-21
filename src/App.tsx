13a45c37c8a13b90113fd7892bbf0d6d22953f65
sophiee <105938870+sophiell27@users.noreply.github.com>
2024-06-21 21:39:40 +0800

    chore: update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Layout from './components/Layout/Layout.tsx';
-import CreatePost from './components/WallView/CreatePost.tsx';
-import WallMainView from './components/WallView/WallMainView.tsx';
-import Wall from './components/WallView/Wall.tsx';
+import CreatePost from './pages/WallMainPage/CreatePostPage.tsx';
+import WallMainView from './pages/WallMainPage/WallMainPage.tsx';
+import Wall from './pages/WallMainPage/WallPage.tsx';
+import LoginView from './pages/LoginPage/LoginPage.tsx';
+import LayoutPage from './pages/LayoutPage/LayoutPage.tsx';
 
 function App() {
   return (
     <Routes>
-      <Route path='/metaWall-w4' element={<Layout />}>
+      <Route path='/metaWall' element={<LayoutPage />}>
         <Route path='' element={<WallMainView />}>
           <Route path='' element={<Wall />} />
           <Route path='newPost' element={<CreatePost />} />
         </Route>
       </Route>
+      <Route path='/metaWall/login' element={<LoginView />}></Route>
     </Routes>
   );
 }
