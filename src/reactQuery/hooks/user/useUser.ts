9e177cf8c46874d4a154fb1df01eb13ef2c1402d
sophiee <105938870+sophiell27@users.noreply.github.com>
2024-06-24 22:16:59 +0800

    feat: update services hooks

--- src/reactQuery/hooks/user/useUser.ts
+++ src/reactQuery/hooks/user/useUser.ts
@@ -5,11 +5,11 @@ import { IUser } from './useUsers';
 const ID = '6653152ac550b8ec12a25c58';
 
 const useUser = () => {
-  const apiClient = new APIClient<IUser>('/users');
+    const apiClient = new APIClient<IUser>('/users/profile');
 
-  return useQuery<IUser, Error>({
-    queryKey: ['user', ID],
-    queryFn: () => apiClient.get(ID),
-  });
+    return useQuery<IUser, Error>({
+        queryKey: ['user', ID],
+        queryFn: () => apiClient.get()
+    });
 };
 export default useUser;
